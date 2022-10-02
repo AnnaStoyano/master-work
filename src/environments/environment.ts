1a4118575761a1f596512fb8b8dd5239842ca3e9
Anna Stoyanova <annstoyanova99@gmail.com>
2022-10-02 13:08:28 +0300

    init app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'master-2022-anna',
+    appId: '1:274019792269:web:45b808b48744fb7ec1f9c4',
+    storageBucket: 'master-2022-anna.appspot.com',
+    apiKey: 'AIzaSyAVe4QqjGblY1oVMejxIJdb6Ya_Ter0rS8',
+    authDomain: 'master-2022-anna.firebaseapp.com',
+    messagingSenderId: '274019792269',
+    measurementId: 'G-4ME8J743G4',
+  },
   production: false
 };
 
